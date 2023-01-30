53876f01cc59cda1d46c11b85c35c686fdcdcd64
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-01-30 09:00:53 +0200

    build: home page route

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: { appDir: true },
-  pageExtensions: ["data.tsx", "data.ts", "data.jsx", "data.js"],
-};
-module.exports = nextConfig;
--- pages/components/ui/Heading.tsx
+++
@@ -1,3 +0,0 @@
-export default function Heading({ title }: any) {
-  return <h2 className="btn-shine">{title}</h2>;
-}

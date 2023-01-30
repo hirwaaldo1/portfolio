095a4478342984d344b73d8ab25abe82f0d989cc
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-01-30 07:42:12 +0200

    build: fix export issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  swcMinify: true,
+  experimental: { appDir: true },
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
+};
+module.exports = nextConfig;
